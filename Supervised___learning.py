469a30a05e7748f622cbcf49a2e50b87d2cefed6
shreyansh7885-ctrl <shreyansh7885@gmail.com>
2026-03-11 19:55:02 +0530

    Add linear regression model for prediction

---
+++ Supervised___learning.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
+y = np.array([35, 40, 50, 55, 65])
+
+model = LinearRegression()
+model.fit(X, y)
+
+prediction = model.predict([[6]])
+
+print("Predicted Marks:", prediction[0])
